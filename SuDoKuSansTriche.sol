f2dd60c8a3ef499d823b325bbe779ba7c524bf02
Arthur <fantache02@gmail.com>
2023-05-31 16:45:51 +0200

    [FEATURE] : SuDoKuSansTriche.sol

--- SuDoKuSansTriche.sol
+++ SuDoKuSansTriche.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.7;
 contract SudokuSolver {
     struct Participant {
         uint256 attempts;
+        uint256 seed;
+        uint256 deposit;
     }
 
     mapping(address => Participant) public participants;
@@ -11,24 +13,32 @@ contract SudokuSolver {
     mapping(bytes32 => bool) public bloomFilterHashes;
     address public owner;
     uint256 public totalAttempts;
+    uint256 public constant DEPOSIT_AMOUNT = 1 ether;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function participate(uint256 attempts, bytes32 bloomFilterHash) public {
+    function participate(uint256 attempts, uint256 seed, bytes32 bloomFilterHash) public payable {
+        require(msg.value == DEPOSIT_AMOUNT, "Il faut déposer 1 pETH.");
         require(bloomFilterHashes[bloomFilterHash] == false, "Ce hash de filtre de Bloom a déjà été soumis.");
         if (participants[msg.sender].attempts == 0) {
             participantAddresses.push(msg.sender);
         }
         participants[msg.sender].attempts += attempts;
+        participants[msg.sender].seed = seed;
+        participants[msg.sender].deposit += msg.value;
         totalAttempts += attempts;
         bloomFilterHashes[bloomFilterHash] = true;
     }
 
+    function verifyAttempts(address participantAddress) public {
+        Participant storage participant = participants[participantAddress];
+    }
+
     function distributeReward() public payable {
-        require(msg.sender == owner, "Seul le propriétaire du smart contract peut distribuer la récompense.");
-        require(msg.value >= totalAttempts, "Le contrat n'a pas assez de pEth pour payer la récompense.");
+        require(msg.sender == owner, "Seul le propriétaire peut distribuer les récompenses.");
+        require(msg.value >= totalAttempts, "Le contrat n'a pas les ressources pour payer la récompense.");
         for(uint256 i = 0; i < participantAddresses.length; i++) {
             address participantAddress = participantAddresses[i];
             payable(participantAddress).transfer((msg.value * participants[participantAddress].attempts) / totalAttempts);
